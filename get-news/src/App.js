62530e1be72c915036488cc78f6c3aa8ed582d96
exogenist <swainson.holness@gmail.com>
2018-07-26 23:27:19 -0600

    state init

--- get-news/src/App.js
+++ get-news/src/App.js
@@ -1,30 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-const key = 'apiKey=6c6b71cbab324fbd82b11f2c79e14456';
-const url = function (endpoint, country, key){
-    return endpoint + country + key;
-};
-const req = new Request(url('https://newsapi.org/v2/top-headlines?','country=us&', key));
-fetch(req).then(function(response){
-    console.log(response.json());
-}); 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+//            {this.props.data.}
+            "Test"
+        );
+    }
 }
+
 
 export default App;
--- get-news/src/index.js
+++ get-news/src/index.js
@@ -4,5 +4,42 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const key = 'apiKey=6c6b71cbab324fbd82b11f2c79e14456';
+const url = function (endpoint, country, key) {
+    return endpoint + country + key;
+};
+const req = new Request(url('https://newsapi.org/v2/top-headlines?', 'country=us&', key));
+
+class Run extends React.Component {
+        render() {
+            return ( < h1 > {
+                    this.props.author
+                } < /h1>);
+            }
+        }
+
+        fetch(req).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+
+            class App extends React.Component {
+                constructor(props) {
+                    super(props);
+                    this.state = {
+                        title: data.articles[0].title
+                    }
+                }
+
+                render(){
+                    return (
+                        <div>
+                            <h1>{this.state.title}</h1>
+                        </div>
+                    )
+                };
+            }
+
+            ReactDOM.render( < App / > , document.getElementById('root'));
+            registerServiceWorker();
+            console.log(data);
+        });
