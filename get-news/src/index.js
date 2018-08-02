3a1d855b9bf51ebc012711eb107732ffb11f8c10
exogenist <swainson.holness@gmail.com>
2018-08-02 13:36:57 -0600

    refactor stage 3 search

--- get-news/src/index.js
+++ get-news/src/index.js
@@ -9,24 +9,23 @@ import registerServiceWorker from "./registerServiceWorker";
 const key = "apiKey=6c6b71cbab324fbd82b11f2c79e14456";
 
 const url = function(endpoint, country, key) {
-  return endpoint + country + key;
+    return endpoint + country + key;
 };
 
 const req = new Request(
-  url("https://newsapi.org/v2/top-headlines?", "country=us&", key)
+    url("https://newsapi.org/v2/top-headlines?", "country=us&", key)
 );
 
-const urlSearch = function(val) {
-  return (
-    "https://newsapi.org/v2/everything?" +
-    "q=" +
-    val +
-    "&" +
-    "from=2018-07-28&" +
-    "language=en&" +
-    "sortBy=popularity&" +
-    key
-  );
+const urlSearch = function(el) {
+    return (
+        "https://newsapi.org/v2/everything?" +
+        "q=" +
+        el +
+        "&" +
+        "language=en&" +
+        "sortBy=popularity&" +
+        key
+    );
 };
 //end
 
@@ -34,112 +33,142 @@ const urlSearch = function(val) {
 // Code for dynamically writing search to fetch -1 ApiCall
 //===========================================
 class MainFrame extends React.Component {
-  constructor(props) {
-    // Load state values
-    super(props);
-    this.state = {
-      value: "",
-      isLoaded: false,
-      getData: null,
-      error: null
-    };
+    constructor(props) {
+        // Load state values
+        super(props);
+        this.state = {
+            value: "",
+            isLoaded: false,
+            getData: null,
+            error: null
+        };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
-
-  handleSubmit(e) {
-    // Render search results here
-
-    e.preventDefault();
-  }
-
-  componentDidMount() {
-    //Make api call for states
-    fetch(req)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(
-        data => {
-          this.setState({
-            isLoaded: true,
-            getData: data
-          });
-        },
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
-  }
-
-  render() {
-    if (this.state.isLoaded === false) {
-      return <div>Loading</div>;
-    } else {
-      var info = this.state.getData;
-      return (
-        //Div container for article feed
-        <div>
-          <div className="container">
-            <div className="row">
-              <nav className="navbar navbar-light">
-                <form className="form-inline" onSubmit={this.handleSubmit}>
-                  <input
-                    className="form-control mr-sm-2"
-                    placeholder="search"
-                    type="search"
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                  />
-                  <button
-                    className="btn btn-outline-success my-2 my-sm-0"
-                    type="submit"
-                  >
-                    search
-                  </button>
-                </form>
-              </nav>
-
-              {info.articles.map(val => (
-                <div
-                  key={info.articles.indexOf(val)}
-                  className="dimention-news-card media col-lg-6 "
-                >
-                  {console.log()}
-                  <div className="inner-container">
-                    <img
-                      className="mr-3 img-size"
-                      src={val.urlToImage}
-                      alt="test"
-                    />
-                    <div className="media-body">
-                      <h4 className="mt-0 card-title">{val.title}</h4>
-                      <p className="mt-1">{val.description}</p>
-                      <a
-                        className="btn btn-primary"
-                        href={val.url}
-                        target="_blank"
-                      >
-                        {val.source.name}
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div> //END
-      );
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    handleSubmit(e) {
+        // Render search results here
+        fetch(urlSearch(this.state.value))
+            .then(function(response) {
+                return response.json();
+            })
+            .then(
+                data => {
+                    this.setState({
+                        isLoaded: true,
+                        getData: data
+                    });
+                },
+                error => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+        e.preventDefault();
+    }
+
+    componentDidMount() {
+        //Make api call for states
+        fetch(req)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(
+                data => {
+                    this.setState({
+                        isLoaded: true,
+                        getData: data
+                    });
+                },
+                error => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+
+
+    }
+
+    RunMe() {
+        return <div>hi</div>
+    }
+
+    render() {
+        if (this.state.isLoaded === false) {
+            return <div>Loading</div>;
+        } else {
+            var info = this.state.getData;
+            return (
+                //Div container for article feed
+                <div>
+                    <div className="container">
+                        <div className="row">
+                            <nav className="navbar navbar-light">
+                                <form
+                                    className="form-inline"
+                                    onSubmit={this.handleSubmit}
+                                >
+                                    <input
+                                        className="form-control mr-sm-2"
+                                        placeholder="search"
+                                        type="search"
+                                        value={this.state.value}
+                                        onChange={this.handleChange}
+                                    />
+                                    <button
+                                        className="btn btn-outline-success my-2 my-sm-0"
+                                        type="submit"
+                                    >
+                                        search
+                                    </button>
+                                </form>
+                            </nav>
+
+                            {info.articles.map(val => (
+                                <div
+                                    key={info.articles.indexOf(val)}
+                                    className="dimention-news-card media col-lg-6 "
+                                >
+                                    {console.log()}
+                                    <div className="inner-container">
+                                        <img
+                                            className="mr-3 img-size"
+                                            src={val.urlToImage}
+                                            alt="test"
+                                        />
+                                        <div className="media-body">
+                                            <h4 className="mt-0 card-title">
+                                                {val.title}
+                                            </h4>
+                                            <p className="mt-1">
+                                                {val.description}
+                                            </p>
+                                            <a
+                                                className="btn btn-primary"
+                                                href={val.url}
+                                                target="_blank"
+                                            >
+                                                {val.source.name}
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div> //END
+            );
+        }
+    }
 }
 
 ReactDOM.render(<MainFrame />, document.getElementById("root"));
